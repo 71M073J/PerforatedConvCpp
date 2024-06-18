1737c35b3b92504c3dd8200482b8661865fa4b5d
71M073J <ciglarictimotej@gmail.com>
2024-06-18 17:42:26 +0200

    add grad_conv, backward compat (useless param)

--- conv.py
+++ conv.py
@@ -45,7 +45,7 @@ class ConvFunction(torch.autograd.Function):
 class PerforatedConv2d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=(1,1), padding=(0,0),
                  stride=1, dilation=1, groups=1, bias=True, device=None,
-                 silent=False, perf_stride=(1,1), upscale_conv=False, strided_backward=False, perforation_mode=None):
+                 silent=False, perf_stride=(1,1), upscale_conv=False, strided_backward=False, perforation_mode=None, grad_conv=None):
         super(PerforatedConv2d, self).__init__()
         if device is None:
             self.device = torch.device("cpu")
