8097d88cf4115a85ed624045c8cad4295768f99a
71M073J <ciglarictimotej@gmail.com>
2024-09-04 22:32:03 +0200

    debugging

--- resnet18-long_test.py
+++ resnet18-long_test.py
@@ -96,10 +96,10 @@ if __name__ == "__main__":
             if type(op) == torch.optim.SGD:
                 lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(op, T_max=epochs)
                 if name.startswith("DAU"):
-                    lr_scheduler = torch.optim.lr_scheduler.SequentialLR(op, [torch.optim.lr_scheduler.LinearLR(op, start_factor=0.0001, total_iters=1),
+                    lr_scheduler = torch.optim.lr_scheduler.SequentialLR(op, [torch.optim.lr_scheduler.LinearLR(op, start_factor=0.0001, total_iters=2),
                                       #torch.optim.lr_scheduler.LinearLR(op, start_factor=0.01, total_iters=1),
                                       #torch.optim.lr_scheduler.LinearLR(op, start_factor=0.01, total_iters=1),
-                                                                              torch.optim.lr_scheduler.CosineAnnealingLR(op, T_max=epochs-1)], milestones=[1])
+                                                                              torch.optim.lr_scheduler.CosineAnnealingLR(op, T_max=epochs-2)], milestones=[2])
             else:
                 epochs = 10
                 eval_mode = [None]
