fix for agriadapt - rotation in segmentation KILLLS weighted IoU due to large non-classified aread

benchmarking.py
@@ -108,7 +108,7 @@ def get_datasets(data, batch_size, augment=True, image_resolution=None):
             num_workers=num_workers, generator=g, )
     elif "agri" in data:
         print(image_resolution)
-        tf.append(transforms.RandomRotation(45))
+        # tf.append(transforms.RandomRotation(45)) #BECAUSE FOR SEGMENTATION GIANT BLACK ANGLES KILL PERFORMANCE!!!!!!!
         tf.append(NormalizeImageOnly([0.4858, 0.3100, 0.3815],
                                      [0.1342, 0.1193, 0.1214]))
         tf = transforms.Compose(tf)
@@ -244,6 +244,9 @@ def train(net, op, data_loader, device, loss_fn, vary_perf, batch_size, perforat
         classes = classes.to(device)
         pred = net(batch)
         loss = loss_fn(pred, classes)
+        if torch.isnan(loss):
+            print("NaN loss reached, quitting...")
+            quit()
         loss.backward()
         losses.append(loss.item())
         if grad_clip is not None:
@@ -274,7 +277,7 @@ def train(net, op, data_loader, device, loss_fn, vary_perf, batch_size, perforat
 
 
 def validate(net, valid_loader, device, loss_fn, file, eval_mode, batch_size, reporting, run_name, dataset):
-    train_mode = None
+
     valid_losses = []
     valid_accs = []
     results = {}
@@ -282,8 +285,6 @@ def validate(net, valid_loader, device, loss_fn, file, eval_mode, batch_size, re
     conf = torch.zeros((sz, sz), device=device)
     # ep_valid_losses = []
     net.eval()
-    if hasattr(net, "_get_perforation"):
-        train_mode = net._get_perforation()
     with torch.no_grad():
         if eval_mode is not None:
             net._set_perforation(eval_mode)
@@ -308,17 +309,15 @@ def validate(net, valid_loader, device, loss_fn, file, eval_mode, batch_size, re
 
                 acc = (softm.argmax(dim=1) == classes)
                 valid_accs.append(torch.sum(acc).detach().cpu() / batch_size)
-
+            if torch.isnan(valid_losses[-1]):
+                print("NaN loss reached, quitting...")
+                quit()
         if reporting:
             if file is not None:
                 print(f"Epoch mean acc: {np.mean(valid_accs).item()}, loss: {np.mean(valid_losses).item()}", file=file)
             print(f"Epoch mean acc: {np.mean(valid_accs).item()}, loss: {np.mean(valid_losses).item()}")
         # ep_valid_losses.append(l2.item() / (i + 1))
-    if train_mode is not None:
-        # print("returning to train mode", train_mode)
-        net._set_perforation(train_mode)
-        # print(train_mode, flush=True)
-        # net._reset()
+
 
     return valid_losses, valid_accs, results, conf.detach().cpu()
 
@@ -337,8 +336,11 @@ def benchmark(net, op, scheduler=None, loss_function=torch.nn.CrossEntropyLoss()
 
     if eval_modes is None:
         eval_modes = (None,)
-    n_conv = len(net._get_n_calc())
-
+    if hasattr(net, "_get_n_calc"):
+        n_conv = len(net._get_n_calc())
+    else:
+        n_conv = 0
+        eval_modes = [None,]
     # net._reset()
     print(f"starting run {run_name}...")
     timeElapsed = 0
@@ -366,12 +368,15 @@ def benchmark(net, op, scheduler=None, loss_function=torch.nn.CrossEntropyLoss()
             print(f"Average Epoch {epoch} Train Loss:", np.mean(losses).item())
             print(f"Epoch mean acc: {np.mean(train_accs).item()}, Epoch time: {timedelta} s")
             print(results)
-
+        train_mode = None
+        if hasattr(net, "_get_perforation"):
+            train_mode = net._get_perforation()
         for ind, mode in enumerate(eval_modes):
 
             print("\ntesting eval mode", mode)
             if file is not None:
                 print("\ntesting eval mode", mode, file=file)
+
             valid_losses, valid_accs, allMetrics, ims = validate(net=net, valid_loader=valid_loader, device=device,
                                                                  loss_fn=loss_function,
                                                                  file=file, batch_size=batch_size, eval_mode=mode,
@@ -381,6 +386,11 @@ def benchmark(net, op, scheduler=None, loss_function=torch.nn.CrossEntropyLoss()
             if curr_loss < best_valid_losses[ind]:
                 best_valid_losses[ind] = curr_loss
                 best_models[ind] = copy.deepcopy(net.state_dict())
+        if train_mode is not None:
+            # print("returning to train mode", train_mode)
+            net._set_perforation(train_mode)
+            # print(train_mode, flush=True)
+            # net._reset()
         if scheduler is not None:
             scheduler.step()
             print(", Current LR:", scheduler.get_last_lr()[0])
@@ -437,7 +447,8 @@ def runAllTests():
             loss_fn = torch.nn.CrossEntropyLoss()
             if dataset == "agri":
                 loss_fn = torch.nn.CrossEntropyLoss(weight=torch.tensor([0.1, 0.9])).to(device)
-                lr = 0.01
+
+                lr = 0.5
                 max_epochs = 300
                 batch_size = 32
             elif dataset == "ucihar":
@@ -478,7 +489,9 @@ def runAllTests():
                                     continue
                                 else:
                                     perf_type = None
-                            prefix = "allTests"
+
+                            prefix = "allTests_2"
+
                             name = f"{modelname}_{dataset}_{img}_{perforation}_{perf_type}"
                             curr_file = f"{name}"
                             if os.path.exists(f"./{prefix}/{curr_file}_best.txt"):
@@ -490,8 +503,6 @@ def runAllTests():
                                             "unet" not in modelname and perf_type != "dau":
                                             #not learning, not resnet
                                             print(f"RE-running run {curr_file}")
-                                        elif type(perforation) == str:#TODO REMOVE
-                                            print("RERUNNING THE RANDOMPERF STUFF, TODO REMOVE")
                                         else:
                                             print("file for", curr_file, "already exists, skipping...")
                                             continue
@@ -501,6 +512,8 @@ def runAllTests():
                             else:
                                 sz = 6 if dataset == "ucihar" else 10
                                 net = model(num_classes=sz).to(device)
+
+
                             pretrained = True #keep default network init
                             if perf[0] is not None:  # do we want to perforate? # Is the net not already perforated?
                                 if type(perf[0]) != str:  # is perf mode not a string
@@ -521,8 +534,14 @@ def runAllTests():
                                 print("Perforating base net for noperf training...")
                                 perfPerf(net, in_size=in_size, perforation_mode=(2,2), pretrained=pretrained)
                                 net._set_perforation((1,1))
-                            # if (dataset == "cifar" and perf_type == "dau") or mo:
-                            #    lr /= 10
+                            if perforation == 2:
+                                eval_modes = [(1, 1), (2, 2), (3, 3), (4, 4)]
+                            elif perforation == 3:
+                                eval_modes = [(1, 1), (2, 2), (3, 3), (4, 4)]
+                            elif perforation is None:
+                                eval_modes = [(1, 1), (2, 2), (3, 3), (4, 4)]
+                            else:
+                                eval_modes = [(1, 1), (2, 2), (3, 3), (4, 4)]
                             print("net:", modelname)
                             print("Dataset:", dataset)
                             print(max_epochs, "epochs")
@@ -534,7 +553,8 @@ def runAllTests():
                             print("Learning rate:", lr)
                             print("run name:", curr_file)
                             # continue
-                            net._reset()
+                            if hasattr(net, "_reset"):
+                                net._reset()
                             net.to(device)
 
                             #TODO make function to eval SPEED, MEMORY, FLOPS of each network, that is called if
@@ -548,6 +568,7 @@ def runAllTests():
                             #                     pretrained=pretrained)
 
 
+
                             op = torch.optim.SGD(net.parameters(), lr=lr, weight_decay=0.0005)
                             train_loader, valid_loader, test_loader = get_datasets(dataset, batch_size, True,
                                                                                    image_resolution=img_res)
@@ -559,10 +580,7 @@ def runAllTests():
                             if not os.path.exists(f"./{prefix}/imgs"):
                                 os.mkdir(f"./{prefix}/imgs")
                             print("starting run:", curr_file)
-                            if perforation == 2 or perforation == 3:
-                                eval_modes[-1] = (4,4)
-                            else:
-                                eval_modes[-1] = (3,3)
+
                             with open(f"./{prefix}/{curr_file}.txt", "w") as f:
                                 best_out, confs, metrics = benchmark(net, op, scheduler, train_loader=train_loader,
                                                             valid_loader=valid_loader, test_loader=test_loader,

hyperparamsearch.py
@@ -1,23 +1,28 @@
 import torch
 from Architectures.UnetCustom import UNet as UNetCustom
-from Architectures.UnetCustom import UNetPerf
-
+#from Architectures.UnetCustom import UNetPerf
+from Architectures.UNetPerf import UNet as UNetPerf
 from benchmarking import benchmark, get_datasets
 from perforateCustomNet import perforate_net_perfconv, add_functs
 weight_decay = 0.0005
 epochs = 100
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
+
 if __name__ == "__main__":
     batch_size = 32
     loss_fn = torch.nn.CrossEntropyLoss(weight=torch.tensor([0.1, 0.9])).to(device)
-    net = UNetPerf(2)
+    net = UNetPerf(2, perforation_mode=(2,2))
+    #net = UNet(2)
     #add_functs(net)
-    perforate_net_perfconv(net, in_size=(2,3,128,128))
-    net._set_perforation((2,2))
-    train_loader, valid_loader, test_loader = get_datasets("agri", batch_size, image_resolution=(128,128))
-    op = torch.optim.SGD(net.parameters(), momentum=0.9, lr=0.1, weight_decay=weight_decay)
+    #perforate_net_perfconv(net, in_size=(2,3,128,128))
+    #net._set_perforation((2,2))
+    op = torch.optim.SGD(net.parameters(), momentum=0.9, lr=0.00001, weight_decay=weight_decay)
+    #op = torch.optim.Adam(net.parameters(), lr=0.0001, weight_decay=0.0005)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(op, T_max=epochs)
+
+    scheduler = torch.optim.lr_scheduler.SequentialLR(op, [torch.optim.lr_scheduler.ConstantLR(op, factor=0.00001), scheduler], milestones=[10])
+    train_loader, valid_loader, test_loader = get_datasets("agri", batch_size, image_resolution=(128,128))
     perf = (2,2)
     run_name = "teeeeeeeeeeest"
     prefix = "h"
@@ -35,6 +40,6 @@ if __name__ == "__main__":
                                                             loss_function=loss_fn,prefix=prefix,
                                                             eval_modes=eval_modes, in_size=in_size, dataset=dataset,
                                                             perforation_type=perf_type, file=f, summarise=False,
-                                                            grad_clip=1.5)
+                                                            grad_clip=None)
     #TODO !!!!!!!
     #try learning networks with non-strided backward! maybe that is the better effect

perforateCustomNet.py
@@ -173,7 +173,7 @@ def replace_module_downActivUp(net, perforation_mode, pretrained=False, from_cla
         layers = get_layers(net)
         start_n = [0]
     if verbose:
-        print([x for x in layers[start_n[0]:start_n[0] + min(len(list(net.named_children())), 10)]])
+        print("Layers applicable currently: ", [x for x in layers[start_n[0]+1:start_n[0] + min(len(list(net.named_children())), 10)]])
     for name, submodule in net.named_children():
         # print("Testing", str(submodule) if type(submodule) != torch.nn.Sequential else "Sequential(...)")
         if type(submodule) == from_class:
@@ -190,6 +190,11 @@ def replace_module_downActivUp(net, perforation_mode, pretrained=False, from_cla
                 print(submodule, layers[start_n[0]][1])
                 print("found layer", str(layers[start_n[0]][1]), "Looking for shape-preserving layers...")
             while not skipWhile:
+                if start_n[0] + cnt >= len(layers):
+                    if verbose:
+                        print("At the end?=")
+                        print(layers, start_n, cnt)
+                    break
                 if any(map(str(layers[start_n[0] + cnt][1]).__contains__, ["Dropout", "Norm", "ReLU", "ELU",
                                                                            "Hardshrink", "Hardsigmoid", "Hardtanh",
                                                                            "Hardswish", "Sigmoid", "SiLU", "Mish",
@@ -256,7 +261,7 @@ def replace_module_downActivUp(net, perforation_mode, pretrained=False, from_cla
                 replace_activs = False
         elif len(list(submodule.children())) != 0:
             if verbose:
-                print("Recursing deeper...")
+                print("Recursing deeper into...", submodule)
             replace_module_downActivUp(submodule, perforation_mode, pretrained, from_class=from_class, layers=layers,
                                        start_n=start_n, replace_activs=replace_activs, verbose=verbose)
         else:
@@ -276,7 +281,9 @@ def _get_perforation(self, part=None):
 
 
 def _set_perforation(self, perfs, part=None, start_n=0):
+
     if part is None:
+        print("Setting perforation mode to ", perfs)
         part = self
         start_n = [0]
     if type(perfs) == tuple:
@@ -307,9 +314,16 @@ def _reset(self):
             if list(c.children()) == [] and hasattr(c, "weight"):
                 return c.weight.device
             else:
+                if hasattr(c, "weight"):
+                    return c.weight.device
                 return find_dev(c)
-        print(net, flush=True)
-        raise AttributeError("No device found?")
+        if list(net.children()) == [] and hasattr(net, "weight"):
+            return net.weight.device
+        else:
+            if hasattr(net, "weight"):
+                return net.weight.device
+            print(net, flush=True)
+            raise AttributeError("No device found?")
 
     if self.training:
         # SPECIFIČNO DAU PROBLEM
@@ -342,7 +356,9 @@ def add_functs(net):
 
 
 def perforate_net_perfconv(net, from_class=torch.nn.Conv2d, perforation_mode=(2, 2), pretrained=True,
-                           in_size=(2, 3, 512, 512)):
+                           in_size=None):
+    if in_size is None:
+        in_size = (2,3,5,5)
     print("perforating network to ", perforation_mode, " perf")
     if len(in_size) == 2:
         in_size = (2, 3, in_size[0], in_size[1])
@@ -354,8 +370,10 @@ def perforate_net_perfconv(net, from_class=torch.nn.Conv2d, perforation_mode=(2,
     net._reset()
 
 
-def perforate_net_downActivUp(net, in_size, from_class=torch.nn.Conv2d, perforation_mode=(2, 2), pretrained=True,
+def perforate_net_downActivUp(net, in_size=None, from_class=torch.nn.Conv2d, perforation_mode=(2, 2), pretrained=True,
                               verbose=False):
+    if in_size is None:
+        in_size = (2,3,5,5)
     if verbose:
         print("perforating network to ", perforation_mode, " DAU")
     if len(in_size) == 2:
@@ -367,3 +385,10 @@ def perforate_net_downActivUp(net, in_size, from_class=torch.nn.Conv2d, perforat
     add_functs(net)
     #print(net.in_size)
     net._reset()
+
+class layerWrapper(torch.nn.Module):
+    def __init__(self, layer):
+        super(layerWrapper, self).__init__()
+        self.layer = layer
+    def forward(self, x):
+        return self.layer(x)

resnet18-long_test.py
@@ -26,25 +26,8 @@ if __name__ == "__main__":
                                             transforms.RandomResizedCrop(size=32)
                                             ]),
                    transforms.RandomHorizontalFlip()])
-    if data == "cinic":
-        tf.append(transforms.Normalize([0.47889522, 0.47227842, 0.43047404],
-                                       [0.24205776, 0.23828046, 0.25874835]))
-        tf_test.append(transforms.Normalize([0.47889522, 0.47227842, 0.43047404],
-                                            [0.24205776, 0.23828046, 0.25874835]))
-        tf = transforms.Compose(tf)
-        tf_test = transforms.Compose(tf_test)
-        dataset1 = DataLoader(CINIC10(partition="train", download=True, transform=tf),  # collate_fn=col,
-                              num_workers=4, batch_size=bs, shuffle=True,
-                              generator=g)
-        dataset2 = DataLoader(
-            CINIC10(partition="valid", download=True, transform=tf_test), num_workers=4,  # collate_fn=col,
-            batch_size=bs, shuffle=True,
-            generator=g, )
-        dataset3 = DataLoader(
-            CINIC10(partition="test", download=True, transform=tf_test), num_workers=4,  # collate_fn=col,
-            batch_size=bs, shuffle=True,
-            generator=g, )
-    elif data == "cifar":
+
+    if data == "cifar":
 
         tf.extend([transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
         tf_test.extend([transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
@@ -66,8 +49,9 @@ if __name__ == "__main__":
     from Architectures.mobilenetv2 import mobilenet_v2 as perfmobilenetv2
     from torchvision.models import resnet18, mobilenet_v2, mobilenet_v3_small
     from perforateCustomNet import perforate_net_downActivUp as DAU
-    for arch, name in [(perfresnet, "perfresnet"), (perfmobilenetv3, "perfmobnetv3"), (perfmobilenetv2, "perfmobnetv2"),
-                       (resnet18, "DAUresnet"), (mobilenet_v3_small, "DAUmobnetv3"), (mobilenet_v2, "DAUmobnetv2")]:
+    for arch, name in [
+                       (resnet18, "DAUresnet"), (mobilenet_v3_small, "DAUmobnetv3"), (mobilenet_v2, "DAUmobnetv2"),
+                        (perfresnet, "perfresnet"), (perfmobilenetv3, "perfmobnetv3"), (perfmobilenetv2, "perfmobnetv2"),]:
         for perf in [(1,1),(2,2),(3,3),"random", "2by2_equivalent"]:
             vary_perf=None
             if type(perf) == str:
@@ -91,11 +75,11 @@ if __name__ == "__main__":
             #op = torch.optim.Adam(net.parameters(), lr=0.001, weight_decay=0.001)
             lr_scheduler = None
             make_imgs = False
-            prefix = "res"
+            prefix = "testFixPerf"
             epochs = 200
             if type(op) == torch.optim.SGD:
                 lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(op, T_max=epochs)
-                if name.startswith("DAU"):
+                if name.startswith("DAU") and False:
                     lr_scheduler = torch.optim.lr_scheduler.SequentialLR(op, [torch.optim.lr_scheduler.LinearLR(op, start_factor=0.0001, total_iters=2),
                                       #torch.optim.lr_scheduler.LinearLR(op, start_factor=0.01, total_iters=1),
                                       #torch.optim.lr_scheduler.LinearLR(op, start_factor=0.01, total_iters=1),